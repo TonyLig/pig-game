7bf27e2c19c6a57e39579d0b89baa4c052b437c9
TonyLig <118136260+TonyLig@users.noreply.github.com>
2023-06-05 22:29:27 +0200

    add player

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting element
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +16,11 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+// Store score and Player
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // Genereting a random dice roll
@@ -20,4 +28,13 @@ btnRoll.addEventListener('click', function () {
   // Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  // Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to next player
+  }
 });
